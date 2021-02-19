2f79ae365b868e0f9d3bff0868796d205ce377b2
omelenetskiy <omelenetskiy@gmail.com>
2021-02-19 14:59:21 +0300

    staging

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>Here is staging branch deploy-3</p>
+        console.log(process.env.NAME);
+        {process.env.NAME === 'John' && (
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+        )}
       </header>
     </div>
   );
